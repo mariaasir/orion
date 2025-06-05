a458b4c7fa43b694b651c40933259bbd9df20faa
UnaiDAM1 <unainieto2004@gmail.com>
2025-06-05 19:41:32 +0000

    Creación del módulo Biblioteca, de su modelo, vistas y CSV

---
+++ orion_documents_library/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Orion Documents Library",
+    "version": "16.0.1.0.0",
+    "category": "Other",
+    "author": "Unai Nieto",
+    "website": "https://git.factorlibre.com",
+    "license": "LGPL-3",
+    "summary": "Description module",
+    "depends": [
+        "base",
+        "orion_base_module",
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        "views/library_views.xml",
+    ],
+    
+
+    "stallable": True,
+    "application": True,
+}
